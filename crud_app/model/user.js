94b22d5e2360cc95a2a2fb41a1081c65df6dea16
koneti rajasekhar <konetirajasekhar1408@gmail.com>
2026-09-11 07:26:08 +0000

    user files is updated

--- crud_app/model/user.js
+++ crud_app/model/user.js
@@ -1,1 +1,16 @@
-console.log("user file open");
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  name: String,
+  email: String,
+  age: Number
+});
+module.exports = mongoose.model('User', userSchema);
+
+
+
